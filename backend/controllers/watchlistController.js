ca96c4b31e8a674b9f010787d3095351f49de8bb
Keegan <keegan.launspach@rewardsco.co.za>
2022-10-06 15:42:17 +0200

    Fully completed watchlist backend calls

--- backend/controllers/watchlistController.js
+++ backend/controllers/watchlistController.js
@@ -1,16 +1,6 @@
-//Add to watched movies
-//Add the to_watch movies
-//Get watched movies based on user ID
-//Get to_watch movies based on user ID
-//Get both watched and to_watch movies on user ID
-//Edit or Delete watched or to_watch record based on user ID
-//Movie can have both true values for watched and to_watch
-
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 const Watchlist = require('../models/watchlistModel')
-
-
 
 // @desc    Get watched
 //@route    GET /api/watchlist/watched
@@ -28,8 +18,8 @@ const getWantToWatch = asyncHandler(async (req, res) => {
     res.status(200).json(watchlist) 
 })
 
-// @desc    Get comments for movie
-//@route    GET /api/commentsForMovie
+// @desc    Get entire watchlist for user
+//@route    GET /api/watchlist
 //@access   Private
 const getWatchlistByUserId = asyncHandler(async (req, res) => {
     const watchlist = await Watchlist.find({ user_id: req.user.id })
@@ -42,19 +32,19 @@ const getWatchlistByUserId = asyncHandler(async (req, res) => {
 })
 
 // @desc    Set Watched
-//@route    POST /api/watchlist/watched
+//@route    POST /api/watchlist
 //@access   Private
-const setWatched = asyncHandler(async (req, res) => {
-    const watchedExists = await Watchlist.findOne({user_id: req.user.id, movie_id: req.body.movie_id});
+const setWatchProps = asyncHandler(async (req, res) => {
+    const watchListRecord = await Watchlist.findOne({user_id: req.user.id, movie_id: req.body.movie_id});
 
-    if (watchedExists) {
+    if (watchListRecord) {
         res.status(400);
-        throw new Error("Movie already exists on watched list.");
+        throw new Error("Movie already exists on list.");
     }
     
-    if(!req.body.watched){
+    if(!req.body.watched && !req.body.wantToWatch){
         res.status(400)
-        throw new Error('Please add a watched field')
+        throw new Error('Please add either a watched or wantToWatch field')
     }
 
     if(!req.user.id){
@@ -70,48 +60,27 @@ const setWatched = asyncHandler(async (req, res) => {
     const comment = await Watchlist.create({
         user: req.user.id,
         movie_id : req.body.movie_id,
-        watched : req.body.watched,
-        wantToWatch : req.body.wantToWatch
+        watched : req.body.watched ? req.body.watched : false,
+        wantToWatch : req.body.wantToWatch ? req.body.wantToWatch : false
     })
     res.status(200).json(comment) 
 })
 
-// @desc    Set goal
-//@route    POST /api/comments
-//@access   Private
-const setWantToWatch = asyncHandler(async (req, res) => {
-    if(!req.body.comment){
-        res.status(400)
-        throw new Error('Please add a comment field')
-    }
-    if(!req.body.rating){
-        res.status(400)
-        throw new Error('Please add a rating field')
-    }
-
-    if(!req.body.movie_id){
-        res.status(400)
-        throw new Error('Please provide movie_id field')
-    }
-
-    const comment = await Watchlist.create({
-        user: req.user.id,
-        comment : req.body.comment,
-        movie_id : req.body.movie_id,
-        rating : req.body.rating
-    })
-    res.status(200).json(comment) 
-})
 
 // @desc    Update goal
-//@route    PUT /api/comments/:id
+//@route    PUT /api/watchlist
 //@access   Private
-const updateWatched = asyncHandler(async (req, res) => {
-    const comment = await Watchlist.findById(req.params.id)
+const updateWatchProps = asyncHandler(async (req, res) => {
+    const watchListRecord = await Watchlist.findById(req.body._id);
 
-    if(!comment){
+    if (!watchListRecord) {
+        res.status(400);
+        throw new Error("Movie does not exist on list.");
+    }
+
+    if(!req.body.watched && !req.body.wantToWatch){
         res.status(400)
-        throw new Error('Goal not found')
+        throw new Error('Please add either a watched or wantToWatch field to update')
     }
 
     //Check for user
@@ -121,51 +90,26 @@ const updateWatched = asyncHandler(async (req, res) => {
     }
 
     //Make sure the logged in user matches the goal user
-    if(comment.user.toString() !== req.user.id){
+    if(watchListRecord.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
-
-    const updatedComment = await Watchlist.findByIdAndUpdate(req.params.id, req.body, {new : true})
-    res.status(200).json(updatedComment) 
+    
+    const updateWatchlist = await Watchlist.findByIdAndUpdate(req.body._id, req.body.watched ? {watched : req.body.watched} : {wantToWatch : req.body.wantToWatch}, {new : true})
+    res.status(200).json(updateWatchlist) 
 })
 
-// @desc    Update goal
-//@route    PUT /api/comments/:id
-//@access   Private
-const updateWantToWatch = asyncHandler(async (req, res) => {
-    const comment = await Watchlist.findById(req.params.id)
-
-    if(!comment){
-        res.status(400)
-        throw new Error('Goal not found')
-    }
-
-    //Check for user
-    if(!req.user){
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-    //Make sure the logged in user matches the goal user
-    if(comment.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('User not authorized')
-    }
-
-    const updatedComment = await Watchlist.findByIdAndUpdate(req.params.id, req.body, {new : true})
-    res.status(200).json(updatedComment) 
-})
 
 // @desc    Delete goal
-//@route    DELETE /api/comments/:id
+//@route    DELETE /api/watchlist
 //@access   Private
-const deleteWatched = asyncHandler(async (req, res) => {
-    const comment = await Watchlist.findById(req.params.id)
+const deleteWatchProps = asyncHandler(async (req, res) => {
+    const watchListRecord = await Watchlist.findById(req.body._id);
+    console.log('watchListRecord:', watchListRecord)
 
-    if(!comment){
-        res.status(400)
-        throw new Error('Watchlist not found')
+    if (!watchListRecord) {
+        res.status(400);
+        throw new Error("Movie does not exist on list.");
     }
 
     //Check for user
@@ -175,50 +119,21 @@ const deleteWatched = asyncHandler(async (req, res) => {
     }
 
     //Make sure the logged in user matches the goal user
-    if(comment.user.toString() !== req.user.id){
+    if(watchListRecord.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized')
     }
 
-    await Watchlist.deleteOne(comment)
-    res.status(200).json({id : req.params.id}) 
+    await Watchlist.deleteOne(watchListRecord)
+    res.status(200).json({id : req.body._id}) 
 })
 
-// @desc    Delete goal
-//@route    DELETE /api/comments/:id
-//@access   Private
-const deleteWantToWatch = asyncHandler(async (req, res) => {
-    const comment = await Watchlist.findById(req.params.id)
-
-    if(!comment){
-        res.status(400)
-        throw new Error('Watchlist not found')
-    }
-
-    //Check for user
-    if(!req.user){
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-    //Make sure the logged in user matches the goal user
-    if(comment.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('User not authorized')
-    }
-
-    await Watchlist.deleteOne(comment)
-    res.status(200).json({id : req.params.id}) 
-})
 
 module.exports = {
  getWatched,
  getWantToWatch,
  getWatchlistByUserId,
- setWatched,
- setWantToWatch,
- updateWatched,
- updateWantToWatch,
- deleteWatched,
- deleteWantToWatch  
+ setWatchProps,
+ updateWatchProps,
+ deleteWatchProps
 }
--- backend/routes/watchlistRoutes.js
+++ backend/routes/watchlistRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getWatched, getWantToWatch, getWatchlistByUserId, setWatched } = require('../controllers/watchlistController')
+const { getWatched, getWantToWatch, getWatchlistByUserId, setWatchProps, updateWatchProps, deleteWatchProps } = require('../controllers/watchlistController')
 const {protect} = require('../middleware/authMiddleware')
 
-router.route('/watched').get(protect, getWatched).post(protect, setWatched)
+router.route('/watched').get(protect, getWatched)
 router.route('/wantToWatch').get(protect, getWantToWatch)
-router.route('/').get(protect, getWatchlistByUserId)
+router.route('/').get(protect, getWatchlistByUserId).post(protect, setWatchProps).put(protect, updateWatchProps).delete(protect, deleteWatchProps)
 
 
 module.exports = router
