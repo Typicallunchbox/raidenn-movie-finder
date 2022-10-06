330bc303b50a99655d5d510e7d0ce732a9433e4a
KEEGAN LAUNSPACH <keegan.launspach1@hotmail.com>
2022-10-06 17:40:52 +0200

    pushed bump to ground

--- frontend/src/components/ThreeJsFiberScreen/Ground.jsx
+++ frontend/src/components/ThreeJsFiberScreen/Ground.jsx
@@ -5,33 +5,39 @@ import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
 
 export function Ground(){
 
-    const [roughness, normal] = useLoader(TextureLoader, [
+    const [roughness, normal, displacement] = useLoader(TextureLoader, [
         process.env.PUBLIC_URL + "textures/rough.png",
         process.env.PUBLIC_URL + "textures/normal.png",
+        process.env.PUBLIC_URL + "textures/bump.png",
+
     ]);
 
     useEffect(() => {
-      [normal, roughness].forEach((t)=>{
+      [normal, roughness, displacement].forEach((t)=>{
         t.wrapS = RepeatWrapping;
         t.wrapT = RepeatWrapping;
         t.repeat.set(5, 5)
       });
 
       normal.encoding = LinearEncoding;
+      displacement.encoding = LinearEncoding;
+
     //   Normally want this encoding for roughness but looks better in this scene without
     //   roughness.encoding = LinearEncoding;
-    }, [normal, roughness])
+    }, [normal, roughness, displacement])
     
 
     
     return (
         <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
-            <planeGeometry args={[30, 30]} />
+            <planeGeometry args={[30, 30, 30, 30]} />
             <MeshReflectorMaterial 
-              envMapIntensity={0}
+              envMapIntensity={1}
               normalMap={normal}
               normalScale={[0.15, 0.15]}
               roughnessMap={roughness}
+              bumpMap={displacement}
+              bumpScale={1}
               dithering={true}
               color={[0.015, 0.015, 0.015, 1]}
               roughness={0.7}
