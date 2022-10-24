6201d29be1649d6610c2aef3fbc3a2a5b3ffd6a4
Keegan <keegan.launspach@rewardsco.co.za>
2022-10-24 07:27:05 +0200

    added icon for del

--- frontend/src/components/ItemCatalogueList/ItemCatalogueList.jsx
+++ frontend/src/components/ItemCatalogueList/ItemCatalogueList.jsx
@@ -4,6 +4,7 @@ import "../ItemCatalogueList/ItemCatalogueList.scss";
 import { updateWatchlistRecord } from "../../features/watchlists/watchlistSlice";
 import {useSelector, useDispatch} from 'react-redux'
 import axios from "axios";
+import { AiFillDelete } from "react-icons/ai";
 
 
 const ItemCatalogueList = (props) => {
@@ -33,7 +34,7 @@ const ItemCatalogueList = (props) => {
         {movies && movies.map((movie) => (
           <>
             <div className="relative" key={movie.id}>
-              {movie.movie_id && <button onClick={() => {removeFromList(movie._id)}}  type="button" className="absolute top-0 right-0 z-10 border-0 text-white bg-red-700 hover:bg-red-800 text-sm sm:w-auto px-6 py-2.5 text-center">Del</button>}       
+              {movie.movie_id && <button onClick={() => {removeFromList(movie._id)}}  type="button" className="absolute top-0 right-0 z-10 border-0 text-white bg-red-700 hover:bg-red-800 text-center p-3 rounded-l-lg"><AiFillDelete/></button>}       
               <img onClick={() => viewMovie(movie.id ? movie.id : movie.movie_id)} src={movie.poster_path ? image_path + movie.poster_path : image_path + movie.movie_image} alt='movie-list'></img>
             </div>
           </>
