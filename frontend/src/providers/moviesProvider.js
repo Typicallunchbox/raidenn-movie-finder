b0fbb24dbc515e67476982aa2114ff2937694d7b
Keegan <keegan.launspach@rewardsco.co.za>
2022-10-25 14:24:27 +0200

    added movies provider

---
+++ frontend/src/providers/moviesProvider.js
@@ -0,0 +1,21 @@
+// import { addTag, addMovies } from '../features/movies/movieSlice';
+// import {useSelector, useDispatch} from 'react-redux'
+// import axios from "axios";
+
+// get movies
+// get movie videos
+// get movies by search
+
+// const moviesProvider = (props) => {
+//     const dispatch = useDispatch();
+
+//    function GetMoviesByPhrase(input){
+//         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=120fe4d587d5f86c44f0a6e599f01734&query=${input}&language=en-US&page=1`)
+//         .then((resp) => {
+//           dispatch(addMovies(resp.data.results))
+//         });
+ 
+//     }
+    
+// }
+// export default moviesProvider
