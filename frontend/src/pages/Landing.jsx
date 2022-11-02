a33802e23302e898f5bdf4b6561e449121d4a5ac
Keegan <keegan.launspach@rewardsco.co.za>
2022-11-02 08:33:36 +0200

    Skeleton file for Landing Page

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import { React, useEffect, useState } from "react";
+
+
+const Landing = () => {
+  function scrollPlay() {
+    // var scrollTop = document.querySelector(".container").scrollTop;
+    // var frameNumber = scrollTop/playbackConst;
+    // vid.currentTime = frameNumber;
+    // window.requestAnimationFrame(scrollPlay);
+  }
+
+  return (
+    <> 
+    <div className="container">
+ 
+    </div>
+    </>
+  );
+};
+export default Landing
