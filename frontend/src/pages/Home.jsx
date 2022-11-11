a5e50c45e72f819e087ceea3071d397b872ee71b
Keegan <keegan.launspach@rewardsco.co.za>
2022-11-11 07:54:44 +0200

    Implemented a movies Provider

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import {useNavigate} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux';
 import ThreeJsFiberScreen from "../components/ThreeJsFiberScreen/ThreeJsFiberScreen";
 import { addMovies } from '../features/movies/movieSlice';
+import {GetPopularMovies, GetMoviesByTag} from "../providers/moviesProvider";
 
 
 // import axios from "axios";
@@ -11,14 +12,11 @@ import ItemCatalogueList from "../components/ItemCatalogueList/ItemCatalogueList
 
 const Home = () => {
 
- 
-
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const {user} = useSelector((state) => state.auth)
-  const { isError, message} = useSelector((state) => state.goals) 
+  const { isError,isLoading, message} = useSelector((state) => state.goals) 
   const {tag, movies} = useSelector((state) => state.movies) 
-  // const [movies, setMovies] = useState([]);
 
   useEffect(() => {
 
@@ -32,46 +30,31 @@ const Home = () => {
   }, [user, navigate, dispatch, isError, message])
 
   useEffect(() => {
-    function scrollPlay() {
-      // var scrollTop = document.querySelector("root").scrollTop;
-      // console.log('val:', scrollTop)
-      // var frameNumber = scrollTop/playbackConst;
-      // vid.currentTime = frameNumber;
-      // window.requestAnimationFrame(scrollPlay);
-    }
-    if(window != undefined){
-        window.addEventListener("scroll", (event) => {
-          let scroll = Math.round(window.scrollY * 100) / 100
-          console.log(scroll)
-      });
+    async function setPopularMovies(){
+      const movies =  await GetPopularMovies();
+      dispatch(addMovies(movies))
     }
 
-    if(tag === ''){
-      axios.get("https://api.themoviedb.org/3/movie/popular?api_key=120fe4d587d5f86c44f0a6e599f01734")
-      .then((resp) => {
-        dispatch(addMovies(resp.data.results))
-      });
-    }else{
-      axios.get(`https://api.themoviedb.org/3/movie/${tag}?api_key=120fe4d587d5f86c44f0a6e599f01734`)
-      .then((resp) => {
-        dispatch(addMovies(resp.data.results))
-      });
+    async function setMoviesByTag(){
+      const movies =  await GetMoviesByTag(tag);
+      dispatch(addMovies(movies))
     }
+
+    tag === '' ? setPopularMovies() : setMoviesByTag();
   }, [tag]);
-
-  
 
   return (
     <> 
     <div className="container">
-    {user && <div className="catalogue mt-52">
-      <ItemCatalogueList movies={movies} />
-    </div>}
-  </div>
-  <div className="recommendations">
-      <ThreeJsFiberScreen />
-  </div>
-  </>
+      {user && <div className="catalogue mt-52">
+        <ItemCatalogueList movies={movies} />
+      </div>}
+    </div>
+    <div className="recommendations">
+        <ThreeJsFiberScreen />
+    </div>
+    </>
   );
 };
+
 export default Home
--- frontend/src/providers/moviesProvider.js
+++ frontend/src/providers/moviesProvider.js
@@ -1,21 +1,30 @@
-// import { addTag, addMovies } from '../features/movies/movieSlice';
-// import {useSelector, useDispatch} from 'react-redux'
-// import axios from "axios";
+import { addTag, addMovies } from '../features/movies/movieSlice';
+import {useSelector, useDispatch} from 'react-redux'
+import axios from "axios";
 
 // get movies
 // get movie videos
 // get movies by search
 
-// const moviesProvider = (props) => {
-//     const dispatch = useDispatch();
+export const GetPopularMovies = async(input) => {
+    return await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=120fe4d587d5f86c44f0a6e599f01734`)
+    .then((resp) => {
+        return resp.data.results.length > 0 ? resp.data.results : []
+    });
+}
 
-//    function GetMoviesByPhrase(input){
-//         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=120fe4d587d5f86c44f0a6e599f01734&query=${input}&language=en-US&page=1`)
-//         .then((resp) => {
-//           dispatch(addMovies(resp.data.results))
-//         });
- 
-//     }
-    
-// }
-// export default moviesProvider
+export const GetMoviesByTag = async(tag) => {
+    console.log('tag:', tag)
+    return await axios.get(`https://api.themoviedb.org/3/movie/${tag}?api_key=120fe4d587d5f86c44f0a6e599f01734`)
+    .then((resp) => {
+        return resp.data.results.length > 0 ? resp.data.results : []
+    });
+}
+
+export const GetMoviesByPhrase = async(input) => {
+    return await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=120fe4d587d5f86c44f0a6e599f01734&query=${input}&language=en-US&page=1`)
+    .then((resp) => {
+        return resp.data.results.length > 0 ? resp.data.results : []
+    });
+}
+
