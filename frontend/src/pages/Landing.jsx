5dcf3ae8e4c02487c351ec545635f357d36260ad
KEEGAN LAUNSPACH <keegan.launspach1@hotmail.com>
2022-12-30 21:32:21 +0200

    Animated Landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,15 +2,42 @@ import { React, useEffect, useState } from "react";
 import placeholder from '../static/images/wire.png'
 
 const Landing = () => {
-  function scrollPlay() {
-    // var scrollTop = document.querySelector(".container").scrollTop;
-    // var frameNumber = scrollTop/playbackConst;
-    // vid.currentTime = frameNumber;
-    // window.requestAnimationFrame(scrollPlay);
-  }
+  // function scrollPlay() {
+  //   var scrollTop = document.querySelector(".container").scrollTop;
+  //   var frameNumber = scrollTop/playbackConst;
+  //   vid.currentTime = frameNumber;
+  //   window.requestAnimationFrame(scrollPlay);
+  // }
+
+  useEffect(() => {
+    var frameNumber = 0,
+    playbackConst = 250, 
+    setHeight = document.getElementById("set-height"), 
+    vid = document.getElementById('v0'); 
+
+    // dynamically set the page height according to video length
+    vid.addEventListener('loadedmetadata', function() {
+      // setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+    });
+
+    // Use requestAnimationFrame for smooth playback
+    function scrollPlay(){  
+      var frameNumber  = window.pageYOffset/playbackConst;
+      vid.currentTime  = frameNumber;
+      window.requestAnimationFrame(scrollPlay);
+    }
+
+    window.requestAnimationFrame(scrollPlay);
+  }, [])
+  
 
   return (
     <> 
+    <div id="set-height"></div>
+    <p id="time"></p>
+    <video id="v0" tabindex="0" autobuffer="autobuffer" preload="preload">
+      <source type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;" src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"></source>
+    </video>
     <div className="container">
       <div className="landing-page">
         <div className="inner-container flex">
