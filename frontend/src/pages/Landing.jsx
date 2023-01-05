deb8c8050ca90d3c7f407e6c8509b7fe63c5c2c0
Keegan <keegan.launspach@rewardsco.co.za>
2023-01-05 15:33:05 +0200

    added some register checks

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,13 @@
 import { React, useEffect, useState, useRef } from "react";
 import placeholder from '../static/images/wire.png';
 import 'animate.css';
+import { Link } from "react-router-dom";
 import landingAnimation from '../static/animations/webRgb.webm';
 
 const Landing = () => {
   const myRef = useRef();
   const [myElementIsVisible, updateMyElementIsVisible] = useState(false);
+
   useEffect(() => {
     var playbackConst = 80, 
     vid = document.getElementById('v0'); 
@@ -27,8 +29,6 @@ const Landing = () => {
     observer.observe(myRef.current);
   }, [])
   
-  
-
   return (
     <> 
     <div id="set-height"></div>
@@ -45,7 +45,9 @@ const Landing = () => {
           </video> 
           </div>
           <div className="relative">
+          <Link to='/register'>
           <button type="button" className={`prim-button w-max absolute bottom-16 left-24 animate__animated animate__fadeInUp animate__delay-2s`}>Get Started</button>
+        </Link>
           </div>
         </div>
       </div>
@@ -54,7 +56,6 @@ const Landing = () => {
           <img onScr src={placeholder} alt='placeholder'></img>
         </div>
         <div className={`setup-content`}>
-         
           <h3 className="text-left ml-14 mb-14">Steps to get started:</h3>
           <div className={`flex mb-5 ${myElementIsVisible ? 'animate__animated animate__fadeInUp animate__delay-1s' : ''}`}>
             <span>1</span>
@@ -68,7 +69,11 @@ const Landing = () => {
             <span>3</span>
             <p className="ml-8 my-auto">Well...that's pretty much it</p>
           </div>
-          <button className={`prim-button mt-14 ${myElementIsVisible ? 'animate__animated animate__fadeInUp animate__delay-3s' : ''}`}>Lets Goooooo!</button>
+          <Link to='/register'>
+            <button className={`prim-button mt-14 ${myElementIsVisible ? 'animate__animated animate__fadeInUp animate__delay-3s' : ''}`}>
+              Lets Goooooo!
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import {toast} from 'react-toastify'
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import {useNavigate} from 'react-router-dom'
-import {useSelector, useDispatch} from 'react-redux'
-import {register, reset} from '../../features/auth/authSlice'
+import {useNavigate} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import {register, reset} from '../../features/auth/authSlice';
+import { validUserName, validEmail, validPasswordStrength } from '../../static/regex'
+
 
 const Login = () => {
 
@@ -48,9 +50,17 @@ const Login = () => {
   const onSubmit = (e) => {
     e.preventDefault()
 
-    if(password !== password2){
-      toast.error('Passwords do not match')
-    }else{
+    if(!validUserName.test(name.trim())){toast.error('Username should be between 4 - 25 Characters (No Special Characters)'); return;}
+    if(!validEmail.test(email.trim())) {toast.error('Enter in a valid email'); return;}
+
+
+    if(!validPasswordStrength.test(password.trim())){
+      toast.error('Please make a more complex password(Uppercase & Lowercase letters, minimum 7 characters, 1 Number & Special character)')
+    }else if(password.trim() !== password2.trim()){
+      toast.error('Passwords do not match')    }
+
+  
+    else{
       const userData = {
         name,
         email,
